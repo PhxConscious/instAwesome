6365cc6868a62428c93f7fce1af6b66c6b04233e
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-11 15:30:42 -0700

    added basic form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className="App">
+      <form className="basicForm" action="#">
+        <div className="mdl-textfield mdl-js-textfield">
+          <input className="mdl-textfield__input" type="text" id="sample1"></input>
+          <label className="mdl-textfield__label" for="sample1">Username</label>
+        </div>
+        <br/>
+        <div className="mdl-textfield mdl-js-textfield">
+          <input className="mdl-textfield__input" type="text" id="sample1"></input>
+          <label className="mdl-textfield__label" for="sample1">Password</label>
+        </div>
+        <br/>
+        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+          Submit
+        </button>
+      </form>
+
+    </div>);
   }
 }
 
