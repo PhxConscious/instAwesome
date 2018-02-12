eeee3df491a186fdf25d693e9c82d1be25c4fc89
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-11 18:38:02 -0700

    setting up firebase on signup form

--- src/Components/Reusable/SignUpForm.js
+++ src/Components/Reusable/SignUpForm.js
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import axios from 'axios';
 
 class SignUpForm extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -16,8 +17,6 @@ class SignUpForm extends Component {
 
         if (email === '' || password === '') {
             return alert('Must fill in all fields')
-        } else if (password.length < 6) {
-            return alert('password must be at least 6 characters long')
         }
         return (
             firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -25,24 +24,36 @@ class SignUpForm extends Component {
                     email: email,
                     user_token: firebase.auth().currentUser.uid,
                 }))
-                .then(() => <h1>Great!</h1>))
-
+                .then(() => console.log('this works')))
     }
 
     render() {
         return (
             <form className="basicForm" action="#">
                 <div className="mdl-textfield mdl-js-textfield">
-                    <input className="mdl-textfield__input" type="text" id="sample1"></input>
-                    <label className="mdl-textfield__label">Username</label>
+                    <input className="mdl-textfield__input"
+                           type="text"
+                           onChange={(email) => {
+                               this.setState({email});
+                               console.log(this.state);
+                            }
+                           }
+                           value={this.state.email}>
+                    </input>
+                    <label className="mdl-textfield__label">Email</label>
                 </div>
                 <br/>
                 <div className="mdl-textfield mdl-js-textfield">
-                    <input className="mdl-textfield__input" type="text" id="sample1"></input>
+                    <input className="mdl-textfield__input"
+                           type="password"
+                           onChange={(password) => this.setState({password})}
+                           value={this.state.password}>
+                    </input>
                     <label className="mdl-textfield__label">Password</label>
                 </div>
                 <br/>
-                <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
+                        onClick={() => this.onButtonPress()}>
                     Submit
                 </button>
             </form>
