c10f1230195acf145436f7dc2611d6329c0c126c
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-11 19:14:49 -0700

    user auth is alive.

--- src/Components/Reusable/SignUpForm.js
+++ src/Components/Reusable/SignUpForm.js
@@ -3,12 +3,11 @@ import firebase from 'firebase';
 import axios from 'axios';
 
 class SignUpForm extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            password: '',
+            password: ''
         };
     }
 
@@ -20,40 +19,47 @@ class SignUpForm extends Component {
         }
         return (
             firebase.auth().createUserWithEmailAndPassword(email, password)
-                .then(() => axios.post('http://localhost:3000/users/new', {
-                    email: email,
-                    user_token: firebase.auth().currentUser.uid,
-                }))
-                .then(() => console.log('this works')))
+        // .then(() => axios.post('http://localhost:3000/users/new', {
+        //     email: email,
+        //     user_token: firebase.auth().currentUser.uid,
+        // }))
+        // .then(() => console.log('this works')))
     }
+
+    handleEmailTextChange = (event) => {
+        this.setState({email: event.target.value})
+    };
+
+    handlePassTextChange = (event) => {
+        this.setState({password: event.target.value})
+    };
 
     render() {
         return (
             <form className="basicForm" action="#">
                 <div className="mdl-textfield mdl-js-textfield">
-                    <input className="mdl-textfield__input"
-                           type="text"
-                           onChange={(email) => {
-                               this.setState({email});
-                               console.log(this.state);
-                            }
-                           }
-                           value={this.state.email}>
+                    <input
+                        className="mdl-textfield__input"
+                        type="text"
+                        onChange={this.handleEmailTextChange}
+                        value={this.state.email}>
                     </input>
                     <label className="mdl-textfield__label">Email</label>
                 </div>
                 <br/>
                 <div className="mdl-textfield mdl-js-textfield">
-                    <input className="mdl-textfield__input"
-                           type="password"
-                           onChange={(password) => this.setState({password})}
-                           value={this.state.password}>
+                    <input
+                        className="mdl-textfield__input"
+                        type="password"
+                        onChange={this.handlePassTextChange}
+                        value={this.state.password}>
                     </input>
                     <label className="mdl-textfield__label">Password</label>
                 </div>
                 <br/>
-                <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-                        onClick={() => this.onButtonPress()}>
+                <button
+                    className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
+                    onClick={() => this.onButtonPress()}>
                     Submit
                 </button>
             </form>
