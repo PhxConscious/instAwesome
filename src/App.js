2de443580f5d8440785364ae2251fd44fb2a48af
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-11 17:07:15 -0700

    added firebase. Made login form its own component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from "./Components/Reusable/LoginForm";
 
 class App extends Component {
   render() {
-    return (<div className="App">
-      <form className="basicForm" action="#">
-        <div className="mdl-textfield mdl-js-textfield">
-          <input className="mdl-textfield__input" type="text" id="sample1"></input>
-          <label className="mdl-textfield__label" for="sample1">Username</label>
-        </div>
-        <br/>
-        <div className="mdl-textfield mdl-js-textfield">
-          <input className="mdl-textfield__input" type="text" id="sample1"></input>
-          <label className="mdl-textfield__label" for="sample1">Password</label>
-        </div>
-        <br/>
-        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-          Submit
-        </button>
-      </form>
-
-    </div>);
+    return (
+        <div className="App">
+          <LoginForm/>
+        </div>);
   }
 }
 
