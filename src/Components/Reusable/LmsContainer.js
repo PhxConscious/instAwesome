73c3ec5014eab65e9faac3390ae5879afcf34f05
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-22 21:31:11 -0700

    set up lms container, lms card, and lms cards.Also added center logo

--- src/Components/Reusable/LmsContainer.js
+++ src/Components/Reusable/LmsContainer.js
@@ -1,8 +1,4 @@
 import React, {Component} from 'react'
-import GreenFormContainerStyles from '../../Styles/GreenFormContainerStyles.css';
-import CompanyInfo from '../Forms/CompanyInfo';
-import ChangePassword from '../Forms/ChangePassword';
-import ContactInfo from '../Forms/ContactInfo';
 
 class LmsContainer extends Component {
     render() {
@@ -10,10 +6,10 @@ class LmsContainer extends Component {
             <div >
                 <div className="mdl-grid lmsOuterContainer">
                     <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet lmsLeftSection">
-
+                        <h1>Left</h1>
                     </div>
                     <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet lmsRightSection">
-
+                        <h1>Right</h1>
                     </div>
                 </div>
             </div>
