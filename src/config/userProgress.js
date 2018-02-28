4910e252b762dc7a983020a59ca5d15d1f3d64c3
Dylan Cunningham <gdylanc@gmail.com>
2018-02-27 20:44:25 -0700

    adds dummy info to simulate database - userProgress

---
+++ src/config/userProgress.js
@@ -0,0 +1,12 @@
+export default {
+  userID: 1234,
+  firstName: "Joe",
+  lastName: "Seph",
+  progress: {
+    aaaaa: true,
+    bbbbb: true,
+    ccccc: false,
+    ddddd: false,
+    eeeee: false
+  }
+}
