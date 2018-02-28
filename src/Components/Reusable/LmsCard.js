9e321810adbbdee44d202f36a1d9f4ad35a19452
Dylan Cunningham <gdylanc@gmail.com>
2018-02-27 17:32:25 -0700

    builds out LmsCard component

--- src/Components/Reusable/LmsCard.js
+++ src/Components/Reusable/LmsCard.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
-
+import '../../Styles/LmsCardStyles.css'
 class LmsCard extends Component {
     render() {
+      const { image, title, description, active, completed, onClick, value } = this.props;
         return(
-            <div>
-
+            <div
+              className={!completed ? "LmsCardContainer locked" : "LmsCardContainer unlocked"}
+              id={active ? "currentCard" : ""}
+              onClick={e => onClick(value)}
+            >
+              <div className="leftSide">
+                <img src={image} className="LmsImage"/>
+              </div>
+              <div className="rightSide">
+                <p className={!completed ? "lmsTitle" : "lmsTitle greyText"}>{title}</p>
+                <p className={!completed ? "lmsDescription" : "lmsDescription greyText"}>{description}</p>
+              </div>
             </div>
         )
     }
