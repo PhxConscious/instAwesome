f282a80be8770c16ddc0bb45056fa3d350bef368
Dylan Cunningham <gdylanc@gmail.com>
2018-02-28 11:51:59 -0700

    adds lessons data to unit data for LMS content

--- src/config/unitCards.js
+++ src/config/unitCards.js
@@ -3,27 +3,75 @@ export default [
     title: "Protip 1: Discovery",
     description: "Find your customers on Instagram, see what they're looking at explore how you can start to show up when they want to see you",
     image: "https://i.imgur.com/EHYS6cp.png",
-    id: "aaaaa"
+    id: "aaaaa",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 1",
+        description: "test video description"
+      }
+    ]
   },{
     title: "Protip 2: Content",
     description: "What makes amazing content? You might be surprised to learn that it's not exactly what you think.",
     image: "https://i.imgur.com/EHYS6cp.png",
-    id: "bbbbb"
+    id: "bbbbb",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 2",
+        description: "test video description"
+      }
+    ]
   },{
     title: "Protip 3: Eat Your Wheaties",
     description: "Breakfast is the healthiest meal of the day and the only you're most likely to not eat. Unless you have a hangover",
     image: "https://i.imgur.com/EHYS6cp.png",
-    id: "ccccc"
+    id: "ccccc",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 3",
+        description: "test video description"
+      }
+    ]
   },{
     title: "Protip 4: Just Do It",
     description: "Remember when everyone wore converse and leather shoes? I don't either and neither does Nike.",
     image: "https://i.imgur.com/EHYS6cp.png",
-    id: "ddddd"
+    id: "ddddd",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 4",
+        description: "test video description"
+      }
+    ]
   },{
     title: "Protip 5: Does A Body Good",
     description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
     image: "https://i.imgur.com/EHYS6cp.png",
-    id: "eeeee"
+    id: "eeeee",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 5",
+        description: "test video description"
+      }
+    ]
+  },{
+    title: "Protip 6: Does A Body Good",
+    description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "fffff",
+    lesson: [
+      {
+        contentType: "video",
+        title: "test video title 6",
+        description: "test video description"
+      }
+    ]
   }
+
 
 ]
