24a2b04d78db03712631419a5c88ee5ab2230c72
Dylan Cunningham <gdylanc@gmail.com>
2018-02-27 20:44:07 -0700

    renames unitCards, which keeps dummy info

--- src/config/unitCards.js
+++ src/config/unitCards.js
@@ -2,23 +2,28 @@ export default [
   {
     title: "Protip 1: Discovery",
     description: "Find your customers on Instagram, see what they're looking at explore how you can start to show up when they want to see you",
-    image: "https://i.imgur.com/EHYS6cp.png"
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "aaaaa"
   },{
     title: "Protip 2: Content",
     description: "What makes amazing content? You might be surprised to learn that it's not exactly what you think.",
-    image: "https://i.imgur.com/EHYS6cp.png"
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "bbbbb"
   },{
     title: "Protip 3: Eat Your Wheaties",
     description: "Breakfast is the healthiest meal of the day and the only you're most likely to not eat. Unless you have a hangover",
-    image: "https://i.imgur.com/EHYS6cp.png"
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "ccccc"
   },{
     title: "Protip 4: Just Do It",
     description: "Remember when everyone wore converse and leather shoes? I don't either and neither does Nike.",
-    image: "https://i.imgur.com/EHYS6cp.png"
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "ddddd"
   },{
     title: "Protip 5: Does A Body Good",
     description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
-    image: "https://i.imgur.com/EHYS6cp.png"
+    image: "https://i.imgur.com/EHYS6cp.png",
+    id: "eeeee"
   }
 
 ]
