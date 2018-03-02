91370c4bfdd130e17773e6e469872f40c8e04058
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 09:36:53 -0700

    updates lesson and dummy user progress data

--- src/config/unitCards.js
+++ src/config/unitCards.js
@@ -1,86 +1,164 @@
+import config from './index.js'
+
 export default [
   {
     title: "Protip 1: Discovery",
     description: "Find your customers on Instagram, see what they're looking at explore how you can start to show up when they want to see you",
     image: "https://i.imgur.com/EHYS6cp.png",
+    video: 'https://www.youtube.com/watch?v=i6G53BMgugo',
     id: "aaaaa",
-    lesson: [
+    lessons: [
       {
-        contentType: "video",
-        title: "Begin your discovery",
+        id: "u01l01",
+        contentType: "checkTasks",
+        title: "testTitle11",
+        description: "watch the damn video",
+        video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
+      },{
+        id: "u01l02",
+        contentType: "checkTasks",
+        title: "testTitle21",
+        description: "watch the damn video",
+        video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
+      },{
+        id: "u01l03",
+        contentType: "checkTasks",
+        title: "testTitle31",
         description: "watch the damn video",
         video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
       }
+      // ,{
+      //   contentType: "multipleChoice",
+      //   title: "Discover some content",
+      //   description: "answer the multipleChoice question",
+      //   id: "testId",
+      //   question: "testQuestion",
+      //   a1: "answer1",
+      //   a2: "answer2",
+      //   a3: "answer3",
+      //   a4: "answer4",
+      //   handler: "handler",
+      //   correct: "answer4"
+      // },{
+      //   contentType: "trueFalse",
+      //   title: "Wheaties are ok",
+      //   description: "answer the question"
+      // }
     ]
   },{
-    title: "Protip 2: Content",
-    description: "What makes amazing content? You might be surprised to learn that it's not exactly what you think.",
+    title: "Protip 2: Discovery",
+    description: "Find your customers on Instagram, see what they're looking at explore how you can start to show up when they want to see you",
     image: "https://i.imgur.com/EHYS6cp.png",
+    video: 'https://www.youtube.com/watch?v=i6G53BMgugo',
     id: "bbbbb",
-    lesson: [
+    lessons: [
       {
-        contentType: "multipleChoice",
-        title: "Discover some content",
-        description: "answer the multipleChoice question",
-        id: "testId",
-        question: "testQuestion",
-        a1: "answer1",
-        a2: "answer2",
-        a3: "answer3",
-        a4: "answer4",
-        handler: "handler",
-        correct: "answer4"
-      }
-    ]
-  },{
-    title: "Protip 3: Eat Your Wheaties",
-    description: "Breakfast is the healthiest meal of the day and the only you're most likely to not eat. Unless you have a hangover",
-    image: "https://i.imgur.com/EHYS6cp.png",
-    id: "ccccc",
-    lesson: [
-      {
-        contentType: "trueFalse",
-        title: "Wheaties are ok",
-        description: "answer the question"
-      }
-    ]
-  },{
-    title: "Protip 4: Just Do It",
-    description: "Remember when everyone wore converse and leather shoes? I don't either and neither does Nike.",
-    image: "https://i.imgur.com/EHYS6cp.png",
-    id: "ddddd",
-    lesson: [
-      {
-        contentType: "video",
-        title: "careful for the swoosh",
-        description: "watch this video"
-      }
-    ]
-  },{
-    title: "Protip 5: Does A Body Good",
-    description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
-    image: "https://i.imgur.com/EHYS6cp.png",
-    id: "eeeee",
-    lesson: [
-      {
-        contentType: "multipleChoice",
-        title: "drink this",
-        description: "answer this question"
-      }
-    ]
-  },{
-    title: "Protip 6: Does A Body Good",
-    description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
-    image: "https://i.imgur.com/EHYS6cp.png",
-    id: "fffff",
-    lesson: [
-      {
-        contentType: "trueFalse",
-        title: "drink more of this",
-        description: "answer some true false questions"
-      }
+        id: "u02l01",
+        contentType: "checkTasks",
+        title: "testTitle1",
+        description: "watch the damn video",
+        video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
+      },{
+        id: "u02l02",
+        contentType: "checkTasks",
+        title: "testTitle2",
+        description: "watch the damn video",
+        video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
+      },{
+        id: "u02l03",
+        contentType: "checkTasks",
+        title: "testTitle3",
+        description: "watch the damn video",
+        video: 'https://www.youtube.com/watch?v=i6G53BMgugo'
+      },
+      // ,{
+      //   contentType: "multipleChoice",
+      //   title: "Discover some content",
+      //   description: "answer the multipleChoice question",
+      //   id: "testId",
+      //   question: "testQuestion",
+      //   a1: "answer1",
+      //   a2: "answer2",
+      //   a3: "answer3",
+      //   a4: "answer4",
+      //   handler: "handler",
+      //   correct: "answer4"
+      // },{
+      //   contentType: "trueFalse",
+      //   title: "Wheaties are ok",
+      //   description: "answer the question"
+      // }
     ]
   }
+  // ,{
+  //   title: "Protip 2: Content",
+  //   description: "What makes amazing content? You might be surprised to learn that it's not exactly what you think.",
+  //   image: "https://i.imgur.com/EHYS6cp.png",
+  //   id: "bbbbb",
+  //   lesson: [
+  //     {
+  //       contentType: "multipleChoice",
+  //       title: "Discover some content",
+  //       description: "answer the multipleChoice question",
+  //       id: "testId",
+  //       question: "testQuestion",
+  //       a1: "answer1",
+  //       a2: "answer2",
+  //       a3: "answer3",
+  //       a4: "answer4",
+  //       handler: "handler",
+  //       correct: "answer4"
+  //     }
+  //   ]
+  // },{
+  //   title: "Protip 3: Eat Your Wheaties",
+  //   description: "Breakfast is the healthiest meal of the day and the only you're most likely to not eat. Unless you have a hangover",
+  //   image: "https://i.imgur.com/EHYS6cp.png",
+  //   id: "ccccc",
+  //   lesson: [
+  //     {
+  //       contentType: "trueFalse",
+  //       title: "Wheaties are ok",
+  //       description: "answer the question"
+  //     }
+  //   ]
+  // },{
+  //   title: "Protip 4: Just Do It",
+  //   description: "Remember when everyone wore converse and leather shoes? I don't either and neither does Nike.",
+  //   image: "https://i.imgur.com/EHYS6cp.png",
+  //   id: "ddddd",
+  //   lesson: [
+  //     {
+  //       contentType: "video",
+  //       title: "careful for the swoosh",
+  //       description: "watch this video"
+  //     }
+  //   ]
+  // },{
+  //   title: "Protip 5: Does A Body Good",
+  //   description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
+  //   image: "https://i.imgur.com/EHYS6cp.png",
+  //   id: "eeeee",
+  //   lesson: [
+  //     {
+  //       contentType: "multipleChoice",
+  //       title: "drink this",
+  //       description: "answer this question"
+  //     }
+  //   ]
+  // },{
+  //   title: "Protip 6: Does A Body Good",
+  //   description: "Who doesn't want to drink a white liquid out of a cow? Pairs nicely with warm cookies.",
+  //   image: "https://i.imgur.com/EHYS6cp.png",
+  //   id: "fffff",
+  //   lesson: [
+  //     {
+  //       contentType: "trueFalse",
+  //       title: "drink more of this",
+  //       description: "answer some true false questions"
+  //     }
+  //   ]
+  // }
 
 
 ]
--- src/config/userProgress.js
+++ src/config/userProgress.js
@@ -3,10 +3,45 @@ export default {
   firstName: "Joe",
   lastName: "Seph",
   progress: {
-    aaaaa: true,
-    bbbbb: true,
-    ccccc: false,
-    ddddd: false,
-    eeeee: false
+    aaaaa: {
+      unitCompleted: true,
+      lessons: [
+        {u01l01: true},
+        {u01l02: true},
+        {u01l03: false}
+      ]
+    },
+    bbbbb: {
+      unitCompleted: false,
+      lessons: [
+        {u02l01: true},
+        {u02l02: false},
+        {u02l03: false}
+      ]
+    },
+    ccccc: {
+      unitCompleted: false,
+      lessons: [
+        {u03l01: true},
+        {u03l02: true},
+        {u03l03: true}
+      ]
+    },
+    ddddd: {
+      unitCompleted: false,
+      lessons: [
+        {lOne: false},
+        {lTwo: false},
+        {lThree: false}
+      ]
+    },
+    eeeee: {
+      unitCompleted: false,
+      lessons: [
+        {lOne: false},
+        {lTwo: false},
+        {lThree: false}
+      ]
+    },
   }
 }
