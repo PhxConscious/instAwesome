d78040c25ce4335743aac52dfc4930e7df28cc58
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 09:37:55 -0700

    updates dashboard to provide currentInitialLesson as props to child

--- src/Components/LMS/Dashboard.js
+++ src/Components/LMS/Dashboard.js
@@ -13,6 +13,8 @@ class Dashboard extends React.Component {
       active: null,
       tasks: [],
       readyForRender: false,
+      currentUnit: '',
+      currentLesson: '',
     }
     this.selectCardOnClick = this.selectCardOnClick.bind(this)
     this.combineUserDataAndTaskData = this.combineUserDataAndTaskData.bind(this);
@@ -45,26 +47,31 @@ class Dashboard extends React.Component {
     tasks.forEach(task => {
       configUnitCards.forEach(card => {
         if(card.id === task) {
+          console.log("task", configUserProgess.progress[task].lessons)
           let key = card.id;
           taskArr.push({
-            task: {
+            userProgress: {
               name: key,
-              isCompleted: configUserProgess.progress[task]
+              isCompleted: configUserProgess.progress[task].unitCompleted,
+              lessons: configUserProgess.progress[task].lessons
             },
             title: card.title,
             description: card.description,
             image: card.image,
             active: false,
-            lesson: card.lesson
+            lessons: card.lessons,
+            id: card.id
           })
         }
       })
     })
 
+    // console.log("taskArr", taskArr)
+
     let activeTask = null;
     // sets the initial active unit
     for(let i = 0; i < taskArr.length; i++){
-      if(taskArr[i].task.isCompleted === false && taskArr[i-1].task.isCompleted === true){
+      if(taskArr[i].userProgress.isCompleted === false && taskArr[i-1].userProgress.isCompleted === true){
         activeTask = taskArr[i].title;
         i = tasks.length;
       }
@@ -81,21 +88,43 @@ class Dashboard extends React.Component {
   // updates state.lesson only when necessary
   componentDidUpdate(prevProps, prevState){
     if(this.state.active !== prevState.active){
-          this.getActiveLesson();
+          this.getInitialActiveLesson();
     }
   }
 
   // puts the active/selected lesson in state
-  getActiveLesson(){
+  getInitialActiveLesson(){
     this.state.tasks.forEach(task => {
       if(task.title === this.state.active){
-        this.setState({
-          ...this.state,
-          currentLesson: task.lesson
+
+        // this block finds the first lesson from userProgress that
+        // isn't complete.
+        let lessons = task.userProgress.lessons;
+        let firstIncompleteLesson = null;
+        for(let i = lessons.length -1; i >= 0; i--){
+          let lessonKey = Object.keys(lessons[i]);
+          if(lessons[i][lessonKey]===false){
+            firstIncompleteLesson=lessonKey[0];
+          }
+        }
+
+        // determines what the inital currentLesson will be
+        task.lessons.forEach((lesson, index) => {
+
+          if (lesson.id === firstIncompleteLesson){
+            this.setState({
+              ...this.state,
+              currentLesson: index.toString()
+            })
+          }
         })
+
       }
     })
+    this.forceUpdate();
   }
+
+
 
   selectCardOnClick(value){
     this.setState({
@@ -120,14 +149,17 @@ class Dashboard extends React.Component {
           onClick={this.selectCardOnClick}
           value={configUnitCards[i].title}
           active={active === configUnitCards[i].title ? true : false}
-          completed={card.task.isCompleted}
+          completed={card.userProgress.isCompleted}
         />
       })
     }
 
-    if (currentLesson) {
-      console.log('lesson', currentLesson, currentLesson[0].title)
-}
+
+
+    // if (currentLesson) {
+    //   console.log('lesson', currentLesson, currentLesson[0].title)
+    // }
+
     return(
       <div className="background">
         <div id="spacer"></div>
@@ -136,7 +168,10 @@ class Dashboard extends React.Component {
         </div>
 
         <div className="lessonContentContainer">
-          {currentLesson ? <LessonContent lesson={currentLesson} /> : 'nope'}
+          {currentLesson ? <LessonContent
+              currentLesson={currentLesson}
+              lesson={configUnitCards}
+            /> : 'nope'}
         </div>
       </div>
     )
