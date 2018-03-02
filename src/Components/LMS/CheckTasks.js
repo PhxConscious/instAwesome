8527af1e99dbea95706fa50090402697a93994dc
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 13:40:12 -0700

    makes buttons enable/disable based on index of lessons array

--- src/Components/LMS/CheckTasks.js
+++ src/Components/LMS/CheckTasks.js
@@ -3,7 +3,7 @@ import React from "react";
 class CheckTasks extends React.Component {
 
   render(){
-    let { lesson, nextLesson, prevLesson, currentUnit, currentLesson } = this.props;
+    let { lesson, nextLesson, prevLesson, currentUnit, currentLesson, noOfLessons } = this.props;
     return(
       <div>
         <div>{lesson.title}</div>
@@ -11,16 +11,28 @@ class CheckTasks extends React.Component {
         <div>{lesson.description}</div>
 
         <div>
-          <button
-            onClick={prevLesson}
-            value="next"
-          >prev</button>
-          <button
-            onClick={nextLesson}
-            value="next"
-          >next</button>
+          <span className={currentLesson === "0" ? "hidden" : ""}>
+            <button
+              onClick={prevLesson}
+              value="next"
+            >prev</button>
+          </span>
+
+          <span className={currentLesson == noOfLessons - 1 ? "hidden" : ""}>
+            <button
+              onClick={nextLesson}
+              value="next"
+            >next</button>
+          </span>
+
         </div>
-        <div>unit: {parseInt(currentUnit)+1}   lesson: {parseInt(currentLesson)+1}</div>
+
+        <div>unit: {parseInt(currentUnit)+1}  current lesson: {parseInt(currentLesson)+1} of {noOfLessons}</div>
+
+        {currentLesson === "0" ? "please begin the lesson" : ''}
+
+        {parseInt(currentLesson)+1 === noOfLessons? "You finished the unit" : ''}
+
       </div>
     )
   }
