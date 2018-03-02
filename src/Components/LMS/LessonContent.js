0cb449e103978066a018b77fa0c32587b560604f
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 09:37:24 -0700

    simplifies lessonContent component

--- src/Components/LMS/LessonContent.js
+++ src/Components/LMS/LessonContent.js
@@ -7,49 +7,57 @@ class LessonContent extends React.Component {
 
     render() {
 
-      const { contentType, title, description, id, question, a1, a2, a3, a4, correct, video } = this.props.lesson[0];
-
+      // const { contentType, title, description, id, question, a1, a2, a3, a4, correct, video } = this.props.lesson;
+      // const { contentType } = this.props;
+        console.log("this.props", this.props)
+        let contentType = "checkTasks";
         switch(contentType){
-          case "video" :
-            return (
-              <VideoContent
-                video={video}
-                title={title}
-                description={description}
-              />
+          // case "video" :
+          //   return (
+          //     <VideoContent
+          //       video={video}
+          //       title={title}
+          //       description={description}
+          //     />
+          //   )
+          //   break;
+
+
+          // case "multipleChoice":
+          //   return (
+          //     <MultipleChoice
+          //     contentType={contentType}
+          //     title={title}
+          //     description={description}
+          //     id={id}
+          //     question={question}
+          //     a1={a1}
+          //     a2={a2}
+          //     a3={a3}
+          //     a4={a4}
+          //     correct={correct}
+          //   />
+          //   )
+          //   break;
+
+
+
+
+          // case "trueFalse":
+          //   return (
+          //     <TrueFalseContent
+          //       title={title}
+          //       description={description}
+          //     />
+          //   )
+          //   break;
+
+
+          case "checkTasks":
+            return(
+              <div>checktasks</div>
             )
             break;
-
-
-          case "multipleChoice":
-            return (
-              <MultipleChoice
-              contentType={contentType}
-              title={title}
-              description={description}
-              id={id}
-              question={question}
-              a1={a1}
-              a2={a2}
-              a3={a3}
-              a4={a4}
-              correct={correct}
-            />
-            )
-            break;
-
-
-
-
-          case "trueFalse":
-            return (
-              <TrueFalseContent
-                title={title}
-                description={description}
-              />
-            )
-            break;
-
 
 
 
