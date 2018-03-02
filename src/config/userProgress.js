9a0b6e6f35743fd28bbe0d3cfa8b0c433d0be483
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 13:37:37 -0700

    updates fixture data

--- src/config/userProgress.js
+++ src/config/userProgress.js
@@ -9,36 +9,44 @@ export default {
       lessons: [
         {u01l01: true},
         {u01l02: true},
-        {u01l03: true}
+        {u01l03: true},
+        {u01l04: true},
+        {u01l05: true},
+        {u01l06: true},
       ]
     },
     bbbbb: {
       unitLocked: false,
-      unitCompleted: false,
+      unitCompleted: true,
       lessons: [
         {u02l01: true},
-        {u02l02: false},
-        {u02l03: false}
+        {u02l02: true},
+        {u02l03: true}
       ]
     },
     ccccc: {
-      unitLocked: true,
+      unitLocked: false,
       unitCompleted: false,
       lessons: [
-        {u03l01: false},
-        {u03l02: false},
-        {u03l03: false}
+        {u03l01: true},
+        {u03l02: true},
+        {u03l03: false},
+        {u03l04: false},
+        {u03l05: false},
+        {u03l06: false},
       ]
     },
     ddddd: {
+      unitLocked: true,
       unitCompleted: false,
       lessons: [
-        {lOne: false},
-        {lTwo: false},
-        {lThree: false}
+        {u04l01: false},
+        {u04l02: false},
+        {u04l03: false}
       ]
     },
     eeeee: {
+      unitLocked: true,
       unitCompleted: false,
       lessons: [
         {lOne: false},
