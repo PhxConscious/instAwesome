8adc714384709849d91f3d25c03ecc3a6f9e3056
Dylan Cunningham <gdylanc@gmail.com>
2018-03-02 20:07:38 -0700

    significant updates to CheckTasks

--- src/Components/LMS/CheckTasks.js
+++ src/Components/LMS/CheckTasks.js
@@ -11,47 +11,50 @@ class CheckTasks extends React.Component {
   render(){
     let { checked } = this.state;
     let { lesson, nextLesson, prevLesson, currentUnit, currentLesson, noOfLessons } = this.props;
-    console.log("lesson", lesson)
-    return(
-      <div>
-        <div>{lesson.title}</div>
 
-        <div>{lesson.description}</div>
-
+    if(lesson && lesson.title){
+      return(
         <div>
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={e => this.setState({checked: !checked})}
-          />
+          <h4>{lesson.title}</h4>
+
+          <div>{lesson.description}</div>
+
+          <div>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={e => this.setState({checked: !checked})}
+            />
+          </div>
+
+          <div>
+            <span className={currentLesson === "0" ? "hidden" : ""}>
+              <button
+                onClick={prevLesson}
+                value="next"
+              >prev</button>
+            </span>
+
+            <span className={parseInt(currentLesson, 10)+1 === parseInt(noOfLessons, 10) ? "hidden" : ""}>
+              <button
+                onClick={nextLesson}
+                value="next"
+                disabled={!checked}
+              >next</button>
+            </span>
+
+          </div>
+
+          <div>unit: {parseInt(currentUnit, 10)+1}  current lesson: {parseInt(currentLesson, 10)+1} of {noOfLessons}</div>
+
+          {currentLesson === "0" ? "please begin the lesson" : ''}
+
+          {parseInt(currentLesson, 10)+1 === noOfLessons? "You finished the unit" : ''}
+
         </div>
-
-        <div>
-          <span className={currentLesson === "0" ? "hidden" : ""}>
-            <button
-              onClick={prevLesson}
-              value="next"
-            >prev</button>
-          </span>
-
-          <span className={parseInt(currentLesson, 10)+1 === parseInt(noOfLessons, 10) ? "hidden" : ""}>
-            <button
-              onClick={nextLesson}
-              value="next"
-              disabled={!checked}
-            >next</button>
-          </span>
-
-        </div>
-
-        <div>unit: {parseInt(currentUnit, 10)+1}  current lesson: {parseInt(currentLesson, 10)+1} of {noOfLessons}</div>
-
-        {currentLesson === "0" ? "please begin the lesson" : ''}
-
-        {parseInt(currentLesson, 10)+1 === noOfLessons? "You finished the unit" : ''}
-
-      </div>
-    )
+      )
+    }
+    return <div>loadin...</div>
   }
 }
 export default CheckTasks;
