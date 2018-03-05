342b56f7a6a74623dcdaccee260f5a7c98c2c190
Dylan Cunningham <gdylanc@gmail.com>
2018-03-04 21:56:31 -0700

    builds action reducers for next question

--- src/redux/actions/userProgress.js
+++ src/redux/actions/userProgress.js
@@ -6,3 +6,11 @@ export const getUserProgress = (fb_id) => {
     payload: axios.get(`http://localhost:8080/users/${fb_id}`)
   }
 }
+
+export const nextQuestion = (fb_id, data) => {
+  console.log('action', fb_id, data)
+  return {
+    type: "NEXT_QUESTION",
+    payload: axios.put(`http://localhost:8080/users/${fb_id}`, data)
+  }
+}
