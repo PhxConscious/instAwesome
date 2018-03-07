937fbe6b65c11caaccd5d514c11ae1a80bb17c80
Dylan Cunningham <gdylanc@gmail.com>
2018-03-07 08:19:17 -0700

    removes console logs

--- src/redux/actions/currentValues.js
+++ src/redux/actions/currentValues.js
@@ -2,7 +2,6 @@
 
 
 export const setCurrentValue = (key, value) => {
-  console.log("action", key, value)
   return {
     type: "SET_CURRENT_VALUE",
     payload: {[key]: value}
--- src/redux/reducers/currentValues.js
+++ src/redux/reducers/currentValues.js
@@ -1,9 +1,7 @@
 export default (state, action) => {
-  console.log("reducer", action.type, action.payload)
   switch (action.type) {
 
     case "SET_CURRENT_VALUE":
-    console.log("fulfilled", action.payload)
       return {
         ...state,
         ...action.payload
