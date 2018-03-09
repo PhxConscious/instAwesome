888f828c628dab8429e6521322837b985cdba907
Dylan Cunningham <gdylanc@gmail.com>
2018-03-09 08:51:57 -0700

    LmsCard: removes unnecessary method and gets values from props

--- src/Components/Reusable/LmsCard.js
+++ src/Components/Reusable/LmsCard.js
@@ -3,44 +3,30 @@ import '../../Styles/LmsCardStyles.css'
 import { connect } from 'react-redux';
 
 class LmsCard extends Component {
-    constructor(props){
-      super(props);
-      this.getCurrentCard = this.getCurrentCard.bind(this);
-    }
 
-    getCurrentCard(){
-      let { active, tasks, currentUnit, currentUnitObj, } = this.props.currentValues;
+    render() {
+      let { image, title, description, active, onClick, userProgress, book, id, index} = this.props;
 
-      let { userProgress, book, id, index } = this.props;
+      let { currentUnit, currentUnitObj, } = this.props.currentValues;
 
       let userProg = userProgress.currentUser.user_progress;
 
-      console.log("currentCard", index, id)
-      // cycle through cards to find which one is current
-
-      //set state values for isLocked and isComplete
-    }
-
-    render() {
-      this.getCurrentCard()
-      const { image, title, description, active, isCompleted, onClick, locked } = this.props;
-
         return(
             <div
-              className={!isCompleted ? "LmsCardContainer locked" : "LmsCardContainer unlocked"}
+              className={!userProg[id].unitCompleted ? "LmsCardContainer locked" : "LmsCardContainer unlocked"}
               id={active ? "currentCard" : ""}
               onClick={e => onClick(title)}
             >
-              <div className="lockIcon"><i className="material-icons">{locked ? "lock" : ""}</i></div>
+              <div className="lockIcon"><i className="material-icons">{userProg[id].unitLocked ? "lock" : ""}</i></div>
               <div className="leftSide">
                 <img src={image} className="LmsImage" alt="blah"/>
               </div>
               <div className="rightSide">
                 <div className="titleCheckboxRow">
-                  <div className="lmsTitleSpan"><p className={isCompleted ? "lmsTitle" : "lmsTitle greyText"} id={active ? "currentCard" : ""}>{title}</p></div>
-                  <div className="lmsCheckboxSpan"><i className="material-icons checkBox">{isCompleted ? "check_box" : "visibility"}</i></div>
+                  <div className="lmsTitleSpan"><p className={userProg[id].unitCompleted ? "lmsTitle" : "lmsTitle greyText"} id={active ? "currentCard" : ""}>{title}</p></div>
+                  <div className="lmsCheckboxSpan"><i className="material-icons checkBox">{userProg[id].unitCompleted ? "check_box" : "visibility"}</i></div>
                 </div>
-                <p className={isCompleted ? "lmsDescription" : "lmsDescription greyText"} id={active ? "currentCard" : ""}>{description}</p>
+                <p className={userProg[id].unitCompleted ? "lmsDescription" : "lmsDescription greyText"} id={active ? "currentCard" : ""}>{description}</p>
               </div>
             </div>
         )
