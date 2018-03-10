0a1c29e42504fa6479e1cfd6f7aefe40b680fb71
Dylan Cunningham <gdylanc@gmail.com>
2018-03-10 14:38:34 -0700

    create new action: createNewUser

--- src/redux/actions/userProgress.js
+++ src/redux/actions/userProgress.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const getUserProgress = (fb_id) => {
+  console.log("Getting user", fb_id)
   return {
     type: "GET_USER_PROGRESS",
     payload: axios.get(`http://localhost:8080/users/${fb_id}`)
@@ -11,5 +12,13 @@ export const nextQuestion = (fb_id, data) => {
   return {
     type: "NEXT_QUESTION",
     payload: axios.put(`http://localhost:8080/users/${fb_id}`, data)
+  }
+}
+
+export const createNewUser = (userObj) => {
+  console.log("Action: createNewUser", userObj);
+  return {
+    type: "CREATE_NEW_USER",
+    payload: axios.post(`http://localhost:8080/users/new`, userObj)
   }
 }
