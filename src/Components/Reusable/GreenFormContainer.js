07af94faa29344e52ebba7c7595b6a8628602484
Dylan Cunningham <gdylanc@gmail.com>
2018-03-12 22:48:06 -0700

    comments out unneeded form. delete ??

--- src/Components/Reusable/GreenFormContainer.js
+++ src/Components/Reusable/GreenFormContainer.js
@@ -1,35 +1,35 @@
-import React, {Component} from 'react'
-
-import Company from '../Forms/Company';
-import ChangePassword from '../Forms/ChangePassword';
-import OnTheWeb from "../Forms/OnTheWeb";
-import PrimaryContact from "../Forms/PrimaryContact";
-
-class GreenFormContainer extends Component {
-    render() {
-        return (
-            <div>
-                <div className="mdl-grid greenFormOuterContainer">
-                    <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet greenFormLeftCont">
-                        <div className='greenFormLeftTop'>
-                            <Company/>
-                        </div>
-                        <div className='greenFormLeftBottom'>
-                            <OnTheWeb/>
-                        </div>
-                    </div>
-                    <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet greenFormRightSection">
-                        <div className='greenFormLeftTop'>
-                            <PrimaryContact/>
-                        </div>
-                        <div>
-                            <ChangePassword/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default GreenFormContainer
+// import React, {Component} from 'react'
+//
+// import Company from '../Forms/Company';
+// import ChangePassword from '../Forms/ChangePassword';
+// import OnTheWeb from "../Forms/OnTheWeb";
+// import PrimaryContact from "../Forms/PrimaryContact";
+//
+// class GreenFormContainer extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <div className="mdl-grid greenFormOuterContainer">
+//                     <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet greenFormLeftCont">
+//                         <div className='greenFormLeftTop'>
+//                             <Company/>
+//                         </div>
+//                         <div className='greenFormLeftBottom'>
+//                             <OnTheWeb/>
+//                         </div>
+//                     </div>
+//                     <div className="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet greenFormRightSection">
+//                         <div className='greenFormLeftTop'>
+//                             <PrimaryContact/>
+//                         </div>
+//                         <div>
+//                             <ChangePassword/>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default GreenFormContainer
