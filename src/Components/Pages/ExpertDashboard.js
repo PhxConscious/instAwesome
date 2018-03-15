20f48e4d641715fbbd46ef92924963061f93c855
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 16:55:05 -0700

    ExpertDashboard: have user data in component

--- src/Components/Pages/ExpertDashboard.js
+++ src/Components/Pages/ExpertDashboard.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Redirect} from 'react-router-dom';
-import { postFeedback } from '../../redux/actions/feedback'
+import { postFeedback } from '../../redux/actions/feedback';
+import { getFreeUsers } from '../../redux/actions/userExpertJoin';
 
 class ExpertDashboard extends React.Component {
   constructor(props){
     super(props)
 
   }
-
+  componentWillMount(){
+    this.props.getFreeUsers()
+  }
 
   render(){
-    const { userInfo } = this.props;
+    const { userInfo, userExpertJoin } = this.props;
 
-    if (typeof(userInfo.currentUser)==='undefined') {
+    if (typeof(userInfo.currentUser)===undefined) {
       return <Redirect to='/'/>
     }
 
+    let freeUsers;
+    if(userExpertJoin && userExpertJoin.freeUsers){
+      freeUsers = userExpertJoin.freeUsers;
+    }
+
+    console.log("freeUsers", freeUsers)
     return (
       <div style={{width: "50vw", margin: "0 auto", marginTop: "100px"}}>
-
+        expert panel
       </div>
     )
   }
@@ -27,13 +36,17 @@ class ExpertDashboard extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    userInfo: state.userProgress.currentUser
+    userInfo: state.userProgress.currentUser,
+    userExpertJoin: state.userExpertJoin
   }
 }
 const mapDispatchToProps = dispatch => {
   return {
     postFeedback: (fb_id, commentObj) => {
       dispatch(postFeedback(fb_id, commentObj))
+    },
+    getFreeUsers: () => {
+      dispatch(getFreeUsers())
     }
   }
 }
