72af08c4707117565c8932a061079151a32db296
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 10:49:51 -0700

    Splash: brings in Redux

--- src/Components/Pages/Splash.js
+++ src/Components/Pages/Splash.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 class Splash extends React.Component {
 
   render(){
+    let { userInfo, companyInfo } = this.props;
+
     return (
-      <div>splash page</div>
+      <div>
+
+
+      </div>
     )
   }
 }
 
-export default Splash;
+const mapStateToProps = state => ({
+  userInfo: state.userProgress,
+  companyInfo: state.companyInfo
+})
+export default connect(mapStateToProps, null)(Splash);
