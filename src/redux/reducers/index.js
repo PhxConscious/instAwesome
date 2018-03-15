275283d14afbd9738e79626b7565e0d976273db4
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 14:07:00 -0700

    adds reducer to combine reducer function

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,13 +4,15 @@ import lmsContent from './lmsContent';
 import currentValues from './currentValues';
 import companyInfo from "./companyInfo";
 import userCompanyJoin from "./userCompanyJoin";
+import feedback from "./feedback";
 
 const rootReducer = combineReducers({
   userProgress: userProgress,
   lmsContent: lmsContent,
   currentValues: currentValues,
   companyInfo: companyInfo,
-  userCompanyJoin: userCompanyJoin
+  userCompanyJoin: userCompanyJoin,
+  feedback: feedback,
 });
 
 export default rootReducer;
