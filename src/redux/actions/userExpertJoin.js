df4611ba144f2cb8cefb3e3f7aca723c45528404
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 16:54:01 -0700

    action: small changes

--- src/redux/actions/userExpertJoin.js
+++ src/redux/actions/userExpertJoin.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getUsersOfExpert = (expert_id) => {
   return {
     type: "GET_USERS_OF_EXPERT",
-    payload: axios.get(`http://localhost:8080/userexpertjoin/users/${expertIs}`)
+    payload: axios.get(`http://localhost:8080/userexpertjoin/users/${expert_id}`)
   }
 }
 
@@ -17,6 +17,6 @@ export const getExpertOfUser = (user_id) => {
 export const getFreeUsers = () => {
   return {
     type: "GET_FREE_USERS",
-    payload: axios.get(`http://localhost:8080/userexpertjoin/getfreeusers)
+    payload: axios.get(`http://localhost:8080/userexpertjoin/freeusers`)
   }
 }
