be7d41cfe8360188c6114d76642805b028578113
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 21:46:23 -0700

    UserListItem&ExpertDashboard: renders more user info upon click

--- src/Components/Pages/ExpertDashboard.js
+++ src/Components/Pages/ExpertDashboard.js
@@ -8,7 +8,7 @@ import UserListItem from '../Admin/UserListItem';
 class ExpertDashboard extends React.Component {
   constructor(props){
     super(props)
-    this.state = {userName: ''}
+    this.state = {userObj: {}}
     this.selectUser = this.selectUser.bind(this);
   }
   componentWillMount(){
@@ -16,12 +16,12 @@ class ExpertDashboard extends React.Component {
   }
 
   selectUser(user){
-    console.log("inselectuser", user)
-    this.setState({userName: user})
+    this.setState({userObj: user})
   }
 
   render(){
     const { userInfo, userExpertJoin } = this.props;
+    let { userObj } = this.state;
 
     if (typeof(userInfo.currentUser)===undefined) {
       return <Redirect to='/'/>
@@ -33,19 +33,25 @@ class ExpertDashboard extends React.Component {
       freeUsers = userExpertJoin.freeUsers;
       unhitchedUsers = freeUsers.map((user, i) => {
         return <UserListItem
-          key={i}
-          user={user}
-          selectUser={this.selectUser}
-        />
+                  key={i}
+                  user={user}
+                  selectUser={this.selectUser}
+                />
       })
     }
 
     if(userExpertJoin && userExpertJoin.freeUsers){
       return (
         <div style={{width: "50vw", margin: "0 auto", marginTop: "100px"}}>
-          expert panel
-          current state: {this.state.userName}
-          {unhitchedUsers}
+          <div style={{display:"inline-block", width: "20vw", "backgroundColor": "yellow"}}>
+            <h6>expert panel</h6>
+            {unhitchedUsers}
+          </div>
+          <div style={{display:"inline-block", width: "30vw", "backgroundColor": "pink"}}>
+            <p>name: {userObj.first_name} {userObj.last_name}</p>
+            <p>email: {userObj.user_email}</p>
+            <p>phone: {userObj.user_phone}</p>
+          </div>
         </div>
       )
     } else {
