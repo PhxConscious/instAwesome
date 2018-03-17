e8fc38fa585e52104a9bd580caeae758e00303e8
Dylan Cunningham <gdylanc@gmail.com>
2018-03-16 19:12:01 -0700

    Main: hooks up AdminDashboard in router

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,6 +10,7 @@ import SignUp from "./Pages/SignUp";
 import Splash from "./Pages/Splash";
 import Feedback from "./Pages/Feedback";
 import ExpertDashboard from "./Pages/ExpertDashboard";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 class Main extends React.Component {
 
@@ -25,7 +26,8 @@ class Main extends React.Component {
                     <Route exact path="/signup" render={props => <SignUp {...props}/>}/>
                     <Route exact path="/splash" render={props => <Splash {...props}/>}/>
                     <Route exact path="/feedback" render={props => <Feedback {...props}/>}/>
-                    <Route exact path="/expertdashboard" render={props => <ExpertDashboard {...props}/>}/>
+                    <Route exact path="/expert/dashboard" render={props => <ExpertDashboard {...props}/>}/>
+                    <Route exact path="/admin/dashboard" render={props => <AdminDashboard {...props}/>}/>
                 </Switch>
             </main>
         )
