faeff9f13567045a680372cdd67d409503c1726d
Dylan Cunningham <gdylanc@gmail.com>
2018-03-17 16:17:01 -0700

    UserOverview: brings in all experts and renders in dropdown menu

--- src/Components/Admin/UserOverview.js
+++ src/Components/Admin/UserOverview.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { IconButton, Menu, MenuItem } from 'react-mdl';
+import { connect } from 'react-redux';
+import { getAllExperts } from '../../redux/actions/userProgress';
 
 class UserOverview extends React.Component {
   constructor(props){
     super(props)
     this.state={}
     this.getCompletedLessons = this.getCompletedLessons.bind(this);
+  }
+
+  componentWillMount(){
+    this.props.getAllExperts();
   }
 
   getCompletedLessons = (units) => {
@@ -21,16 +28,47 @@ class UserOverview extends React.Component {
 
 
   render(){
+    let { expertList } = this.props.users;
+    let theExperts;
+    console.log("suser", this.props.users)
+    if(this.props.users.expertList){
+      theExperts = expertList.map(expert => {
+        return (
+          <MenuItem>
+            {expert.first_name}
+          </MenuItem>
+        )
+      })
+    }
+
     let { user } = this.props;
-    console.log("user", user)
+    // console.log("user", user)
     return(
       <div>
         <p>{user.first_name} {user.last_name}</p>
         <p>{user.user_email}</p>
         <p>{user.user_phone}</p>
         <ul>{this.getCompletedLessons(user.user_progress).map(lesson => <li>lesson</li>)}</ul>
+        <div style={{position: 'relative'}}>
+          <IconButton name="more_vert" id="demo-menu-top-left" />
+          <Menu target="demo-menu-top-left" valign="bottom" ripple>
+              {theExperts}
+          </Menu>
+        </div>
       </div>
     )
   }
 }
-export default UserOverview;
+
+const mapStateToProps = state => ({
+  users: state.userProgress
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllExperts: () => {
+      dispatch(getAllExperts())
+    },
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(UserOverview);
