ba2c756b872a209f6d4734c15fd447488c2eeda1
Dylan Cunningham <gdylanc@gmail.com>
2018-03-18 22:05:36 -0700

    new class method moved to utils.helper

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,19 +1,32 @@
 export const getCompletedQuestionStatus = data => {
-    let total = 0;
-    let completed = 0;
-    for(let unit in data){
-      for(let key in data[unit]){
-        if(key === "lessons"){
-          for(let ky in data[unit][key]){
-            for(let k in data[unit][key][ky].questions){
-              if(data[unit][key][ky].questions[k]===true){
-                completed ++;
-              }
-              total ++;
+  let total = 0;
+  let completed = 0;
+  for(let unit in data){
+    for(let key in data[unit]){
+      if(key === "lessons"){
+        for(let ky in data[unit][key]){
+          for(let k in data[unit][key][ky].questions){
+            if(data[unit][key][ky].questions[k]===true){
+              completed ++;
             }
+            total ++;
           }
         }
       }
     }
-    return Math.round((completed / total)*100)
   }
+  return Math.round((completed / total)*100)
+}
+
+
+export const getCompletedLessons = (units) => {
+  let result = [];
+  for(let unit in units){
+    for(let lesson in units[unit].lessons){
+      if(units[unit].lessons[lesson].lessonCompleted === true){
+        result.push(lesson)
+      }
+    }
+  }
+  return result;
+}
