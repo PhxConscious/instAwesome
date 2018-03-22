f78547f396b7914d991fa6544950457b7fff12b1
Ismael Perez <ismaelperezemail@gmail.com>
2018-03-22 11:58:01 -0700

    PrimaryContactForm: form now interacting with db.

--- src/Components/Forms/PrimaryContact.js
+++ src/Components/Forms/PrimaryContact.js
@@ -1,31 +1,37 @@
 import React, {Component} from "react";
 import '../../Styles/FormsStyles.css';
+import {connect} from 'react-redux';
+import {updateCompanyInfo} from "../../redux/actions/companyInfo";
 
 class PrimaryContact extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            fullName: '',
-            phoneNumber: '',
-            emailAddress: '',
+            primary_contact_full_name: '',
+            primary_contact_phone_number: '',
+            primary_contact_email: '',
             error: '',
             loading: false
         };
     }
 
-    onButtonPress() {
-        this.setState({error: '', loading: true});
+    onButtonPress(e) {
+        e.preventDefault();
+        this.setState({error: ''});
         console.log('button works and displays loading...')
+        this.props.addCompanyInfo(this.props.companyInfo.companyList && this.props.companyInfo.companyList[0].company_id, this.state);
+        if (this.props.companyInfo.status !== 200) {
+            return alert('update failed')
+        } else {
+            return alert('update successful')
+        }
     }
 
     renderButton() {
-        if (this.state.loading) {
-            return <p className=""></p>
-        }
         return (
             <button
                 className="formButton"
-                onClick={() => this.onButtonPress()}>
+                onClick={(e) => this.onButtonPress(e)}>
                 <span className='buttonText'>
                     UPDATE
                 </span>
@@ -51,12 +57,12 @@ class PrimaryContact extends Component {
                                 <p className='inputLabel'>FULL NAME</p>
                             </div>
                             <input
-                                name='fullName'
+                                name='primary_contact_full_name'
                                 className="formInput"
                                 type="text"
                                 onChange={this.handleInputTextChange}
                                 placeholder='Jeremy Chevallier'
-                                value={this.state.fullName}>
+                                value={this.state.primary_contact_full_name}>
                             </input>
                         </div>
                         <div className="formInputCont">
@@ -64,12 +70,12 @@ class PrimaryContact extends Component {
                                 <p className='inputLabel'>PHONE NUMBER</p>
                             </div>
                             <input
-                                name='phoneNumber'
+                                name='primary_contact_phone_number'
                                 className="formInput"
                                 type="text"
                                 onChange={this.handleInputTextChange}
                                 placeholder='480-268-5305'
-                                value={this.state.phoneNumber}>
+                                value={this.state.primary_contact_phone_number}>
                             </input>
                         </div>
                         <div className="formInputCont">
@@ -77,12 +83,12 @@ class PrimaryContact extends Component {
                                 <p className='inputLabel'>EMAIL ADDRESS</p>
                             </div>
                             <input
-                                name='emailAddress'
+                                name='primary_contact_email'
                                 className="formInput"
                                 type="text"
                                 onChange={this.handleInputTextChange}
                                 placeholder='Jeremy@phxconscious.com'
-                                value={this.state.emailAddress}>
+                                value={this.state.primary_contact_email}>
                             </input>
                         </div>
                     </div>
@@ -94,4 +100,17 @@ class PrimaryContact extends Component {
     }
 }
 
-export default PrimaryContact;
+const mapStateToProps = state => ({
+    currentValues: state.currentValues,
+    companyInfo: state.companyInfo
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addCompanyInfo: (companyId, companyObj) => {
+            dispatch(updateCompanyInfo(companyId, companyObj))
+        }
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PrimaryContact);
