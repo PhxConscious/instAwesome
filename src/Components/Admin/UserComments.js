0a3af9a54dac416853173d9e8d75c21c86057da2
Dylan Cunningham <gdylanc@gmail.com>
2018-03-27 19:36:17 -0700

    UserComments: renders comments by user

--- src/Components/Admin/UserComments.js
+++ src/Components/Admin/UserComments.js
@@ -1,31 +1,60 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getFeedbackByUserId } from '../../redux/actions/feedback';
+import moment from 'moment';
 
 class UserComments extends React.Component {
   constructor(props){
     super(props)
     this.state = {};
+  }
+
+  componentWillMount(){
+    this.props.getFeedbackByUserId(this.props.user.firebase_id)
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.user !== this.props.user){
+      this.props.getFeedbackByUserId(this.props.user.firebase_id)
+    }
 
   }
+
   render(){
-    let { user } = this.props;
+    let { user, userComments } = this.props;
+    let theComments;
+    if(userComments){
+
+      theComments = userComments.map((comment, i) => {
+        let time = moment(comment.created_at).fromNow();
+        return <div
+            key={i}
+          >
+            <span>{comment.comment}</span>
+            <span style={{float:"right", fontSize:".8em"}}>{time}</span>
+          </div>
+      })
+    }
 
     return (
       <div>
-      UserComments
-      {user.first_name}
+      {theComments}
       </div>
     )
   }
 }
-// const mapStateToProps = state => ({
-//
-// })
+const mapStateToProps = state => ({
+  // if(feedback && feedback.allCommentsCurUser){
+    userComments: state.feedback.allCommentsCurUser
+  // }
+})
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//
-//   }
-// }
+const mapDispatchToProps = dispatch => {
+  return {
+    getFeedbackByUserId: (fb_id) => {
+      dispatch(getFeedbackByUserId(fb_id));
+    }
+  }
+}
 
-export default connect(null, null)(UserComments);
+export default connect(mapStateToProps, mapDispatchToProps)(UserComments);
