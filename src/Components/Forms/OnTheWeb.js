c2563d080f14a78f0199a32d85523f554cef0cc0
Ismael Perez <ismaelperezemail@gmail.com>
2018-03-31 15:16:09 -0700

    OnTheWeb: Minor syntax changes.

--- src/Components/Forms/OnTheWeb.js
+++ src/Components/Forms/OnTheWeb.js
@@ -41,12 +41,13 @@ class OnTheWeb extends Component {
             better_business_bureau_goals: '',
             errorWarnings: [],
             isError: false,
-            snackBarActive:false,
+            snackBarActive: false,
         };
         // this.pullInUserValues = this.pullInUserValues.bind(this);
         this.validateFBUrl = this.validateFBUrl.bind(this);
         this.validateTwitterUsername = this.validateTwitterUsername.bind(this);
     }
+
 
     componentDidMount() {
         let {companyInfo} = this.props;
@@ -85,27 +86,28 @@ class OnTheWeb extends Component {
                 better_business_bureau_goals: companyInfo.companyList[0].better_business_bureau_goals
             })
         }
-
     }
+
 
     onButtonPress(e) {
         e.preventDefault();
         this.setState({
-          errorWarnings: [],
-          snackBarActive:false,
-        })
+            errorWarnings: [],
+            snackBarActive: false,
+        });
         this.validateFBUrl(this.state.facebook_page_url);
         this.validateTwitterUsername(this.state.twitter_username);
         let me = this;
 
-        setTimeout(function(){
-          if(me.state.errorWarnings.length === 0) {
-            me.props.addCompanyInfo(me.props.companyInfo.companyList && me.props.companyInfo.companyList[0].company_id, me.state)
-          } else {
-            me.setState({snackBarActive:true})
-          }
+        setTimeout(function () {
+            if (me.state.errorWarnings.length === 0) {
+                me.props.addCompanyInfo(me.props.companyInfo.companyList && me.props.companyInfo.companyList[0].company_id, me.state)
+            } else {
+                me.setState({snackBarActive: true})
+            }
         }, 500)
     }
+
 
     renderButton() {
         if (!this.state.loading) {
@@ -121,60 +123,63 @@ class OnTheWeb extends Component {
         }
     }
 
+
     handleInputTextChange = e => {
         this.setState({[e.target.name]: e.target.value});
     };
 
+
     validateFBUrl = (fb) => {
-      let base = "https://facebook.com/"
+        let base = "https://facebook.com/"
 
-      // validates anything with correct base or empty string
-      if((fb.substring(0,21) === base || fb.length === 0)){
-        return true;
-      }
+        // validates anything with correct base or empty string
+        if ((fb.substring(0, 21) === base || fb.length === 0)) {
+            return true;
+        }
 
-      let warning = 'Your facebook url should look like this: https://facebook.com/yourUserName. \n Enter your facebook url or leave this field blank.'
+        let warning = 'Your facebook url should look like this: https://facebook.com/yourUserName. \n Enter your facebook url or leave this field blank.'
 
-      let newArr = this.state.errorWarnings;
+        let newArr = this.state.errorWarnings;
 
-      if(!this.state.errorWarnings.includes(warning)){
-        newArr = this.state.errorWarnings.concat(warning);
-      }
-      this.setState({errorWarnings: newArr, isError: true})
+        if (!this.state.errorWarnings.includes(warning)) {
+            newArr = this.state.errorWarnings.concat(warning);
+        }
+        this.setState({errorWarnings: newArr, isError: true})
+    };
+
+
+    validateTwitterUsername(twit) {
+        if ((twit.substring(0, 1) === "@" && twit.length >= 3 && !twit.includes(" ")) || twit.length === 0) {
+            return true;
+        }
+
+        let warning = "Your twitter user name should begin with '@' and have no spaces";
+        let newArr = this.state.errorWarnings;
+
+        if (!this.state.errorWarnings.includes(warning)) {
+            newArr = this.state.errorWarnings.concat(warning);
+        }
+
+        this.setState({errorWarnings: newArr, isError: true})
     }
 
-    validateTwitterUsername(twit){
-      if((twit.substring(0,1)==="@" && twit.length >= 3 && !twit.includes(" ")) || twit.length === 0){
-        return true;
-      }
-
-      let warning = "Your twitter user name should begin with '@' and have no spaces";
-      let newArr = this.state.errorWarnings;
-
-      if(!this.state.errorWarnings.includes(warning)){
-        newArr = this.state.errorWarnings.concat(warning);
-      }
-
-      this.setState({errorWarnings: newArr, isError: true})
-    }
 
     render() {
         return (
             <div>
                 <Grid>
-
                     <Cell col={8} offsetDesktop={2} tablet={12} phone={12}>
-                      <Snackbar
-                        style={{width:"100%"}}
-                        active={this.state.snackBarActive}
-                        onClick={e=>this.setState({isError:false, errorWarnings:[], snackBarActive:false})}
-                        onTimeout={e=>this.setState({snackBarActive: false})}
-                        action="OK"
-                      >
-                        {this.state.errorWarnings.map((warning, i)=>{
-                          return <div key={i}>* {warning}</div>
-                        })}
-                      </Snackbar>
+                        <Snackbar
+                            style={{width: "100%"}}
+                            active={this.state.snackBarActive}
+                            onClick={e => this.setState({isError: false, errorWarnings: [], snackBarActive: false})}
+                            onTimeout={e => this.setState({snackBarActive: false})}
+                            action="OK"
+                        >
+                            {this.state.errorWarnings.map((warning, i) => {
+                                return <div key={i}>* {warning}</div>
+                            })}
+                        </Snackbar>
                         <form className="formCont" action="#">
                             <div className='inputCont'>
                                 <div className="formInputCont">
@@ -476,11 +481,13 @@ class OnTheWeb extends Component {
     }
 }
 
+
 const mapStateToProps = state => ({
     currentValues: state.currentValues,
     userFbId: state.currentValues.currentFbId,
     companyInfo: state.companyInfo,
 });
+
 
 const mapDispatchToProps = dispatch => {
     return {
