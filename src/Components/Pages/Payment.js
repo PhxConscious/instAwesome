093e2c55afb5b518174c29cd427503ebaf174b88
Dylan Cunningham <gdylanc@gmail.com>
2018-04-02 16:19:58 -0700

    Payment: cleans up and adds makePayment dispatch

--- src/Components/Pages/Payment.js
+++ src/Components/Pages/Payment.js
@@ -1,38 +1,66 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
-import config from '../../config';
+import React from 'react';
+import { connect } from 'react-redux';
+import BuyInsta3200 from '../Payment/BuyInsta3200';
+import Receipt from '../Payment/Receipt';
 
-export default class TakeMoney extends React.Component {
+class Payment extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
+      clicked: false,
+    }
+    this.printReceipt = this.printReceipt.bind(this);
+    this.showPurchaseScreen = this.showPurchaseScreen.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.payments.currentPayment !== this.props.payments.currentPayment){
+      this.setState({clicked: true})
     }
   }
 
-  onToken = (token) => {
-    console.log('post token', token)
-    axios.post(`${config.app.api}/payment`, {token})
-    .then(response => {
-      console.log("response", response)
-      alert(`We are in business, ${response.data[0].email}`);
-    });
-  }
-
-  render() {
+  printReceipt(){
+    let { currentPayment } = this.props.payments;
     return (
       <div>
-        <StripeCheckout
-          name="instawesome cashpoint"
-          description="must enter to win"
-          amount={320000}
-          token={this.onToken}
-          zipCode={true}
-          billingAddress={true}
-          stripeKey="pk_test_fpE4GmaHdmv1wf4XhUIU1bAL"
-          locale="auto"
-        />
+        <Receipt payment={currentPayment}/>
       </div>
     )
   }
+
+  showPurchaseScreen(){
+    return (
+
+      <div style={{display:"flex", justifyContent:"center", backgroundColor:"#f3f3f3", height:"90vh"}}>
+        <div style={{ margin:"10vh 0 10vh 0", flexDirection:"column", backgroundColor:"white", width:"60vw", padding:"0 10px 0 10px"}}>
+          <h3>Do you agree to sign up?</h3>
+          <p>Terms and conditions here</p>
+          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+          <BuyInsta3200 />
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    let { clicked } = this.state;
+    if(this.props.userInfo &&  this.props.userInfo.firebase_id){
+      return (
+        <div>
+          {clicked ? this.printReceipt() : this.showPurchaseScreen()}
+        </div>
+      )
+    } else {
+      return <div>Please login</div>
+    }
+
+  }
 }
+
+const mapStateToProps = state => ({
+  userInfo: state.userProgress.currentUser,
+  payments: state.payments
+})
+
+export default connect(mapStateToProps, null)(Payment);
