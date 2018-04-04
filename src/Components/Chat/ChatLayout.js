c9b32971504610650f2233af873129fc5be364b1
Dylan Cunningham <gdylanc@gmail.com>
2018-04-04 08:38:29 -0700

    creates ChatLayout: connects to socketio

---
+++ src/Components/Chat/ChatLayout.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import io from 'socket.io-client';
+
+const socketUrl = "http://localhost:3231";
+
+export default class ChatLayout extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      socket:null,
+    }
+  }
+
+  componentWillMount(){
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    const socket = io(socketUrl)
+
+    socket.on('connect', ()=>{
+      console.log("connected in checkLayout")
+    })
+
+    this.setState({socket})
+  }
+
+  render(){
+    const {title} = this.props;
+    return (
+      <div>{title}</div>
+    )
+  }
+}
+
+;
