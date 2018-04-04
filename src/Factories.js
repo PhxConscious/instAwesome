3e70dd08afcf9fe64a3e2611e50ead3babfee02d
Dylan Cunningham <gdylanc@gmail.com>
2018-04-04 09:20:32 -0700

    created Factories:

---
+++ src/Factories.js
@@ -0,0 +1,28 @@
+// creates unique id for all users
+const uuidv4 = require("uuid/v4");
+
+
+const createUser = ({name:""} = {})=> ({
+  id: uuidv4(),
+  name
+
+})
+
+const createMessage = ({message:"", sender:""} = {})=>({
+  id: uuidv4(),
+  time: getTime(new Date(Date.now())),
+  message,
+  sender
+})
+
+const getTime = (date) =: {
+  return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
+}
+
+createChat = ({messages = [], name = "Community", users = []} = {})=>({
+  id: uuidv4(),
+  name,
+  messages,
+  users,
+  typingUsers: []
+})
